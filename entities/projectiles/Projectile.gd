f497fabfc1d7d079a6e3489231a1e3b7bfe88905
Anatole de Chauveron <54862351+Anatole-DC@users.noreply.github.com>
2026-01-21 16:59:16 +0100

    Feat/curve controlled slides (#10)
    * wip(movement): curve-controlled speed
    
    * wip(projectile): transfer speed to projectile
    
    * feat(movement): better movement

--- entities/projectiles/Projectile.gd
+++ entities/projectiles/Projectile.gd
@@ -4,7 +4,6 @@ var spawn_position: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print("Spawing new projectile")
 	global_position = spawn_position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,4 +13,3 @@ func _process(_delta):
 
 func _on_projectile_lifetime_timer_timeout():
 	queue_free()
-	print("Projectile destroyed")
